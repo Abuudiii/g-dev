36d595a513ba4a92fd2b41714937fca96b2b3a14
Abdullah Sheikh <absheikh@qnx.com>
2025-12-11 16:18:59 -0500

    updated code to actual practice code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
 
+
+float parseAndCalculate(std::string& expression) {
+    float a = 0.f;
+    float b = 0.f;
+    float result = 0.f;
+    bool seenOperand = false;
+    char operand;
+
+    for (const auto& x : expression) {
+
+        if (isdigit(x)) {
+            if(!seenOperand) {
+                a = a * 10 + (x - '0');
+            } else {
+                b = b * 10 + (x - '0');
+            }
+        }
+
+        if (x == '+' || x == '-' || x == '/' || x == '*') {
+            seenOperand = true;
+            operand = x;
+        }
+
+    }
+    
+    switch (operand)
+    {
+    case '+':
+        result = a + b;
+        return static_cast<int>(result);
+
+    case '-':
+        result = a - b;
+        return static_cast<int>(result);
+        break;
+
+    case '/':
+        result = a / b;
+        return result;
+        break;
+
+    case '*':
+        result = a * b;
+        return result;
+        break;
+    
+    default:
+        std::cout << "Invalid Operand: " << operand << std::endl;
+        break;
+    }
+
+    return 0;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string expression;
+
+    std::cout << "Enter equation: " << std::endl;
+    getline(std::cin, expression);
+
+    auto result = parseAndCalculate(expression);
+
+    std::cout << result << std::endl;
+
     return 0;
 }
